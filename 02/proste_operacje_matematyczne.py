402903105262278d403b354a8e32ee81ead1cc52
KrzysztofMisztal <krzysztof.misztal@dicella.com>
2024-08-29 21:26:18 +0200

    tydzień 3

---
+++ 02/proste_operacje_matematyczne.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+
+suma = a + b
+roznica = a - b
+iloczyn = a * b
+iloraz = a / b
+
+print("Suma:", suma)  # > Suma: 15
+print("Różnica:", roznica)  # > Różnica: 5
+print("Iloczyn:", iloczyn)  # > Iloczyn: 50
+print("Iloraz:", iloraz)  # > Iloraz: 2.0
---
+++ 02/zmienne.py
@@ -0,0 +1,9 @@
+liczba_calkowita = 7
+liczba_zmiennoprzecinkowa = 3.14
+napis = "Programowanie jest fajne!"
+wartosc_logiczna = True
+
+print(liczba_calkowita)  # > 7
+print(liczba_zmiennoprzecinkowa)  # > 3.14
+print(napis)  # > Programowanie jest fajne!
+print(wartosc_logiczna)  # > True
